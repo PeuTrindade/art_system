e24e3e57d2b1d5e2c1c2d68244a87b1d540c9dfb
Pedro Trindade <84353169+PeuTrindade@users.noreply.github.com>
2024-05-26 12:54:02 -0300

    feat: adding auth route

--- src/interfaces/User/IUser.ts
+++ src/interfaces/User/IUser.ts
@@ -2,6 +2,7 @@ export interface IUser {
     id: number | string
     name: string
     email: string
+    password: string
     age: number
     style: string
 }
--- src/routes/Feedback.routes.ts
+++ src/routes/Feedback.routes.ts
@@ -1,9 +1,10 @@
 import express from "express"
 import FeedbackController from "../controllers/FeedbackController"
+import Auth from "../middlewares/Auth"
 
 const feedbackRoutes = express.Router()
 
-feedbackRoutes.get('/:id', FeedbackController.list)
-feedbackRoutes.post('/', FeedbackController.create)
+feedbackRoutes.get('/:id', Auth as any, FeedbackController.list)
+feedbackRoutes.post('/', Auth as any, FeedbackController.create)
 
 export { feedbackRoutes } 
